8dade6775de9bff0b4473ab16b65fa6552b53e0b
OverKrice3000 <bober.xxl@yandex.ru>
2022-03-15 00:41:07 +0700

    uploading finished task

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,22 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    let map = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+
+    for (let i = 0; i < str.length - 1; i++) {
+        if (map[str.charAt(i)] < map[str.charAt(i + 1)]) {
+            result -= map[str.charAt(i)];
+        } else {
+            result += map[str.charAt(i)];
+        }
+    }
+    result += map[str.charAt(str.length - 1)];
     return result;
 }
